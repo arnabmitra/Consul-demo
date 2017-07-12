53c2bb813c8098f1545c6c31de514e5392231138
Arnab Mitra <amitra@ip-192-168-4-219.us-west-2.compute.internal>
2017-07-12 10:08:17 -0600

    Correcting stuff.

--- consul-client/src/main/java/com/amitra/consulclient/PetsController.java
+++ consul-client/src/main/java/com/amitra/consulclient/PetsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +22,7 @@ public class PetsController {
     RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/{name}")
-    public String[] getPets(@PathParam("name") String petName)
+    public String[] getPets(@PathVariable("name") String petName)
     {
         ServiceInstance serviceInstance=client.getInstances("pet-service")
                 .stream().findFirst().
